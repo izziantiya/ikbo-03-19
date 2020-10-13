871403bab2e5c57a1169fbbc532e15c7bf0bf689
izziantiya1 <56535563+izziantiya1@users.noreply.github.com>
2020-10-13 16:33:25 +0300

    когда-то все было так просто

---
+++ mirea/PR1/Dog.java
@@ -0,0 +1,18 @@
+package mirea.PR1;
+
+public class Dog {
+    private String name;
+    private int age;
+    private String color;
+
+    public Dog(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Собака: " + "возраст\t" + age + ", имя\t" + name + ", цвет\t" + color;
+    }
+}
